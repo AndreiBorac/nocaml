d30238ac0582a204bab423fce9d75ba35f53ef38
Andrei Borac <aborac@nocaml.com>
2020-04-01 19:28:57 -0700

    Weekly update
     - added big- and little-endian packer functions
     - added Vector5
     - improved blob primordial support
     - added blob list primordial builder

--- stdlib/packer.c
+++ stdlib/packer.c
@@ -38,6 +38,62 @@ WOMBAT_BUILTIN static uintptr_t* wombat_builtin_packer_minus_pack_minus_32(Womba
   return cellb;
 }
 
+WOMBAT_BUILTIN static uintptr_t* wombat_builtin_packer_minus_pack_minus_16_minus_el(WombatExternal* wombat_external WOMBAT_UNUSED, uintptr_t* wombat_context WOMBAT_UNUSED, uintptr_t* cella, uintptr_t* cellb)
+{
+  PACKER_CHECK_TOP(cellb);
+  assure((cella[0] == WOMBAT_NATIVE_CONSTRUCTOR_Integer));
+  uintptr_t bit = (1UL << ((8*sizeof(uintptr_t))-1));
+  assure((cellb[0] & bit) != 0);
+  uintptr_t len = (cellb[0] - bit);
+  assure(len >= 2);
+  ((uint8_t*)(&(cellb[1])))[0] = ((uint8_t)(cella[1]      ));
+  ((uint8_t*)(&(cellb[1])))[1] = ((uint8_t)(cella[1] >>  8));
+  return cellb;
+}
+
+WOMBAT_BUILTIN static uintptr_t* wombat_builtin_packer_minus_pack_minus_32_minus_el(WombatExternal* wombat_external WOMBAT_UNUSED, uintptr_t* wombat_context WOMBAT_UNUSED, uintptr_t* cella, uintptr_t* cellb)
+{
+  PACKER_CHECK_TOP(cellb);
+  assure((cella[0] == WOMBAT_NATIVE_CONSTRUCTOR_Integer));
+  uintptr_t bit = (1UL << ((8*sizeof(uintptr_t))-1));
+  assure((cellb[0] & bit) != 0);
+  uintptr_t len = (cellb[0] - bit);
+  assure(len >= 4);
+  ((uint8_t*)(&(cellb[1])))[0] = ((uint8_t)(cella[1]      ));
+  ((uint8_t*)(&(cellb[1])))[1] = ((uint8_t)(cella[1] >>  8));
+  ((uint8_t*)(&(cellb[1])))[2] = ((uint8_t)(cella[1] >> 16));
+  ((uint8_t*)(&(cellb[1])))[3] = ((uint8_t)(cella[1] >> 24));
+  return cellb;
+}
+
+WOMBAT_BUILTIN static uintptr_t* wombat_builtin_packer_minus_pack_minus_16_minus_be(WombatExternal* wombat_external WOMBAT_UNUSED, uintptr_t* wombat_context WOMBAT_UNUSED, uintptr_t* cella, uintptr_t* cellb)
+{
+  PACKER_CHECK_TOP(cellb);
+  assure((cella[0] == WOMBAT_NATIVE_CONSTRUCTOR_Integer));
+  uintptr_t bit = (1UL << ((8*sizeof(uintptr_t))-1));
+  assure((cellb[0] & bit) != 0);
+  uintptr_t len = (cellb[0] - bit);
+  assure(len >= 2);
+  ((uint8_t*)(&(cellb[1])))[0] = ((uint8_t)(cella[1] >>  8));
+  ((uint8_t*)(&(cellb[1])))[1] = ((uint8_t)(cella[1]      ));
+  return cellb;
+}
+
+WOMBAT_BUILTIN static uintptr_t* wombat_builtin_packer_minus_pack_minus_32_minus_be(WombatExternal* wombat_external WOMBAT_UNUSED, uintptr_t* wombat_context WOMBAT_UNUSED, uintptr_t* cella, uintptr_t* cellb)
+{
+  PACKER_CHECK_TOP(cellb);
+  assure((cella[0] == WOMBAT_NATIVE_CONSTRUCTOR_Integer));
+  uintptr_t bit = (1UL << ((8*sizeof(uintptr_t))-1));
+  assure((cellb[0] & bit) != 0);
+  uintptr_t len = (cellb[0] - bit);
+  assure(len >= 4);
+  ((uint8_t*)(&(cellb[1])))[0] = ((uint8_t)(cella[1] >> 24));
+  ((uint8_t*)(&(cellb[1])))[1] = ((uint8_t)(cella[1] >> 16));
+  ((uint8_t*)(&(cellb[1])))[2] = ((uint8_t)(cella[1] >>  8));
+  ((uint8_t*)(&(cellb[1])))[3] = ((uint8_t)(cella[1]      ));
+  return cellb;
+}
+
 WOMBAT_BUILTIN static uintptr_t* wombat_builtin_packer_minus_unpack_minus_8(WombatExternal* wombat_external WOMBAT_UNUSED, uintptr_t* wombat_context WOMBAT_UNUSED, uintptr_t* cella, uintptr_t* cellb)
 {
   PACKER_CHECK_TOP(cellb);
